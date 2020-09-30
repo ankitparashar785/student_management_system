f24e4eca020452cf7e638a16eeb9496b1e01c27f
Ankit parashar <62975799+ankitparashar785@users.noreply.github.com>
2020-09-30 19:34:15 +0700

    Update attandancedetail.php

--- attandancedetail.php
+++ attandancedetail.php
@@ -31,29 +31,17 @@ $id= $_SESSION['id'];
 </head>
 <body>
   <div class="container">
-  <input id="hidin" type="hidden" name=""value="<?echo $id;?>">
+  <input id="hidin" type="hidden" name=""value=<?php echo $id;?>>
   <div class="text-center mt-5">
     <h1 class="text-center text-danger "> ATTANDENCE DETAILS</h1>
     <label>From </label>
-<input type="date" name="" >
+<input type="date" name="" id="s_date" >
 <label>  To </label>
-<input type="date" name="">
+<input type="date" name="" id="l_date">
   </div>
   <button class="btn btn-success"  id="attan">SHOW</button>
     <div class="table-responsive-sm mt-5 table-success" id="table">
-   <table class='table'>
-
-    <tbody>
-    <tr>
-      <th scope='row'>$s</th>
-      <td>$rows['std_name'];</td>
-      <td> $rows['date_a'];</td>
-      <td>$rows['status'];</td>
-    </tr>
-  </tbody>
- 
-  </table>
-    
+       
 
   </div>
 </div>
@@ -61,27 +49,31 @@ $id= $_SESSION['id'];
 	
 $(document).ready(function (e) {
 	// body...
-	  $("#table").hide();
-	$("#attan").on("click",function() {
-      
+	  
+	$("#attan").on("click",function(e) {
+      e.preventDefault();
+     
       var id=$("#hidin").val();
-   
-        
-      $.ajax({
+     var s=$("#s_date").val();
+     var l=$("#l_date").val();
+      if (s!=''&&l!='') {
 
+      	   $.ajax({ 
        url:"showattandance.php",
        type:"POST",
-       data:{std_id=id},
+       data:{std_id:id,start_date:s,end_date:l},
        success:function(data) {
     
-
+        $("#table").html(data);
 
        }
-
-
-
       });
-
+      }
+      else
+      {
+      	alert('Select date');
+      }
+       
 	});
 });
 
